3f1d1e28de4fe9b9dc597bab2066291968545ddb
hasanhabsyi2011 <71365610+hasanhabsyi2011@users.noreply.github.com>
2020-09-18 10:50:13 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,24 @@
+var nim = 123
+var nama = "UPIN"
+var statusNikah = true
+var kosong = null
+var undifined
+var dataKeluarga = ["ipin","upin","ros","mail"] 
+
+console.log(nim)
+console.log(nama)
+console.log(statusNikah)
+console.log(kosong)
+console.log(undifined)
+console.log(dataKeluarga)
+
+console.log(typeof nim)
+console.log(typeof nama)
+console.log(typeof statusNikah)
+console.log(typeof kosong)
+console.log(typeof undifined)
+console.log(typeof dataKeluarga)
+
+
+
+
